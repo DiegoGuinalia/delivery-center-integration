3b9f70abdaec6792df83038e34a90fd910de5490
Agostinho Neto <agostinhomartinsneto@gmail.com>
2021-02-07 23:46:46 -0300

    configured rspec to running tests in project

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -1,7 +1,20 @@
 ENV['RACK_ENV'] = 'test'
 
-require "../lib/app"
+require './lib/api'
+require 'rack/test'
+require 'factory_bot'
+require 'mongoid-rspec'
+
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
+
+  config.include Mongoid::Matchers
+
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryBot.find_definitions
+  end
 end
+
