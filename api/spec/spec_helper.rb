649605d8d692de59dbbfdd8d220854d3a632f030
Agostinho Neto <agostinhomartinsneto@gmail.com>
2021-02-08 23:57:45 -0300

    added database_cleaner-mongoid gem to clean the database between tests to avoid data inconsistency

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'rack/test'
 require 'factory_bot'
 require 'mongoid-rspec'
 require 'vcr'
+require 'database_cleaner-mongoid'
 
 # Load support
 Dir["#{Dir.pwd}/spec/support/**/*.rb"].sort.each { |file| require file }
@@ -18,6 +19,15 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     FactoryBot.find_definitions
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :deletion
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
   end
 end
 
